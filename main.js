f1a68408a5994c65a9a24d700e43b3a80b2c516b
Santosh Maurya <83776025+Joker-pyc@users.noreply.github.com>
2023-08-17 12:26:49 +0530

    Updated the joystick for mouse usage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import VirtualJoystick from "https://joker-pyc.github.io/Virtual-Joystick/src/virtual-joystick.min.js";
+import VirtualJoystick from "https://github.com/Joker-pyc/Virtual-Joystick/blob/main/src/virtual-joystick.js";
 
 // Define constants for the game
 const GAME_WIDTH = window.innerWidth;
